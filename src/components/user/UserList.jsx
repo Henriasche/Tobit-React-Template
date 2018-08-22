a2311e34ef06a30080ef74b1cf53eab951720535
Henri Asche <henri.asche@tobit.software>
2018-08-22 09:54:58 +0200

    Restructure Fileorder

--- src/components/user/UserList.jsx
+++ src/components/user/UserList.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Accordion } from 'chayns-components';
-import User from './user/User';
+import { Accordion } from 'chayns-components/lib';
+import User from './userElement/User';
 
 /**
  * Whenever the user prop gets a new value this component re-renders. Users were added in the Content.jsx via the PersonFinder
+ * If a Component has no States (State-less Component) you should Change the Component to a Function
  */
 export default class UserList extends React.Component {
 
@@ -24,6 +25,7 @@ export default class UserList extends React.Component {
             <Accordion head="User List" defaultOpened>
                 <div className="accordion__content" id="usersList">
                     {
+                        // You can write some JavaScript in this HTML-Code if it is inside {}
                         user && user.map(({ name, userId }) => (
                             <User
                                 key={userId}
